a471443c63d1b6d570586cd86180b01719081c65
random-zebra <random.zebra@protonmail.com>
2020-03-12 16:50:36 +0100

    Merge #1375: [Init] Do parameter interaction before creating the UI model
    9182b9007559fa6ae945b1e13984dc1971572c28 Initialize logging before we do parameter interaction (Jonas Schnelli)
    dec078739aca63be8dc7c6e2fbb8267c2dfa86c3 [QT] Call inits parameter interaction before we create the options model (Jonas Schnelli)
    18480ce73994d9326b27a0ff7a034e71ec5f0dce Refactor parameter interaction, call it before AppInit2() (Jonas Schnelli)
    
    Pull request description:
    
      Backport of https://github.com/bitcoin/bitcoin/pull/6780
    
      Currently optionsModel(QT) parameters interaction overwrites/dominates initial and depending parameter settings. This PR would factor out init's parameter interaction and call it before AppInit2().
    
    ACKs for top commit:
      furszy:
        ACK 9182b90
      random-zebra:
        ACK rebase 9182b9007559fa6ae945b1e13984dc1971572c28 and merging...
    
    Tree-SHA512: f01c9b6603452d59c3fa7df8b8f85998b9c716fd3209f80fc6d4591035c995ad577b217d1d7ffd7f642d16fe130ee922ba0394870ec391b8e972587022962611

--- src/init.cpp
+++ src/init.cpp
@@ -906,6 +906,26 @@ void InitParameterInteraction()
     }
 }
 
+void InitLogging()
+{
+    fPrintToConsole = GetBoolArg("-printtoconsole", false);
+    fLogTimestamps = GetBoolArg("-logtimestamps", true);
+    fLogIPs = GetBoolArg("-logips", false);
+
+    LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    LogPrintf("PIVX version %s (%s)\n", FormatFullVersion(), CLIENT_DATE);
+}
+
+/** Initialize pivx.
+ *  @pre Parameters should be parsed and config file should be read.
+ */
+bool AppInit2()
+{
+    // ********************************************************* Step 1: setup
+    if (!AppInitBasicSetup())
+        return false;
+
+    // ********************************************************* Step 2: parameter interactions
     // Make sure enough file descriptors are available
     int nBind = std::max((int)mapArgs.count("-bind") + (int)mapArgs.count("-whitebind"), 1);
     nMaxConnections = GetArg("-maxconnections", 125);
