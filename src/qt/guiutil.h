7651cb76ba758d5fe2690e31c39ebae12e41b241
akshay <akshaycm@hotmail.com>
2020-03-22 16:50:11 +0300

    Merge #1392: [GUI] Accept dialogs with ENTER, reject with ESC
    37e73fe6b14f2e78fef729cdd0a6282f0284dc48 [Doc] Add GUI dailogs keyboard navigation to the release notes (random-zebra)
    52157cececf85f4d191e4414b854bc79bcf56bd1 [GUI] Guard access to TxDetailDialog::acceptTx (random-zebra)
    7ec6c134ca2e9307d6249f768f0e03f2f615b3ec [GUI] Set focus in DefaultDialog and detect Enter/Esc (random-zebra)
    436fea2bc22d5e450fc3f4f3afb475bb3ecf68cd [GUI] Set focus in TxDetailDialog and detect Enter/Esc (random-zebra)
    
    Pull request description:
    
      Ref: https://github.com/PIVX-Project/PIVX/pull/1163#pullrequestreview-328980277
    
      Completes the ability to accept/dismiss modal dialogs with the keyboard, including:
      - confirmation dialogs (`DefaultDialog`)
      - tx detail / tx confirmation dialogs (`TxDetailDialog`)
    
    ACKs for top commit:
      furszy:
        ACK 37e73fe
    
    Tree-SHA512: 0d0771e8b9891a5d3555c835564feadee2dd4f9d23b0733361e9279448f559eee1865369693d428e210c40f48ad80c544f757c4ec01cdf6f7f57a999e3e2a056

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -32,7 +32,17 @@ class QUrl;
 class QWidget;
 QT_END_NAMESPACE
 
-/** Utility functions used by the ZENZO Qt UI.
+/*
+ * General GUI exception
+ */
+class GUIException : public std::exception
+{
+public:
+    std::string message;
+    GUIException(const std::string &message) : message(message) {}
+};
+
+/** Utility functions used by the PIVX Qt UI.
  */
 namespace GUIUtil
 {
